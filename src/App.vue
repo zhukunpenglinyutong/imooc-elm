ac977eff32dcb0dadca7202d127e3094a3126e7b
zhukunpenglinyutong <270750933@qq.com>
2019-06-03 23:33:44 +0800

    第一次提交（2019.6.3 23:33）| 第一天学习

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+  </div>
+</template>
+
+<script>
+import VHeader from './components/v-header/v-header'
+
+export default {
+  name: 'app',
+  components: {
+    VHeader
+  }
+}
+</script>
